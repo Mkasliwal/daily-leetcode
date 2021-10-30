71b839016bb4184d430119bb8396b0ceeabb156f
mkasliwal <mats.kasliwal@gmail.com>
2021-10-30 15:49:02 +0530

    single number

---
+++ arrays/single-number/single-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> m;
+        
+        for(int i=0; i<nums.size(); i++) {
+            m[nums[i]]++; 
+        }
+        int ans = 0;
+        unordered_map<int, int>::iterator it;
+        for(it = m.begin(); it != m.end(); it++) {
+            if(it->second<2) {
+                ans = it->first;
+                break;
+            }
+        }
+        return ans;
+    }
+};
+
